1bab832c6be35e0db139c2c7e000ea0aea5e9618
Donovan Cary <donovancary2005@gmail.com>
2025-11-18 12:15:40 -0500

    Added on to Search.js, still needs html implementation

--- Search.js
+++ Search.js
@@ -1,34 +1,64 @@
 // This file is for searching through the data base to find the selected country and corresponding history from the query thing lol
 
-function search(queryExample){
-    console.log("reached search function");
-    switch (queryExample){
+function searchCountry(countryQuery){
+    console.log("reached searchCountry function");
+    switch (countryQuery){
         case 0:
-            queryExample.toLowerCase = "united states" || "united states of america" || "us" || "u.s." || "u.s.a"
+            countryQuery.toLowerCase = "united states" || "united states of america" || "us" || "u.s." || "u.s.a"
             console.log("USA selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 1:
-            queryExample.toLowerCase = "united kingdom" || "great britian" || "uk" || "england"
+            countryQuery.toLowerCase = "united kingdom" || "great britian" || "uk" || "england"
             console.log("UK selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 2:
-            queryExample.toLowerCase = "russia" || "soviet union" || "ussr"
+            countryQuery.toLowerCase = "russia" || "soviet union" || "ussr"
             console.log("Russia selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 3:
-            queryExample.toLowerCase = "japan"
+            countryQuery.toLowerCase = "japan"
             console.log("Japan selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 4:
-            queryExample.toLowerCase = "china" || "people's republic of china" || "p.r.c"
+            countryQuery.toLowerCase = "china" || "people's republic of china" || "p.r.c"
             console.log("China selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 5:
-            queryExample.toLowerCase = "germany"
+            countryQuery.toLowerCase = "germany"
             console.log("Germany selected");
-            return queryExample + " selected";
+            return countryQuery + " selected";
         case 6:
             console.log("selection not implemented or doesn't exist");
             return "This selection has not been implemented or doesn't exist. Please try again."
+    }
+}
+
+function searchHistoryType(historyQuery){
+    console.log("Reached searchHistoryType function")
+    switch (historyQuery){
+        case 0:
+            historyQuery.toLowerCase = "military" || "military history" || "warfare" || "wars" // theres too many words...
+            console.log("Military History selected");
+            return historyQuery + "selected"; 
+        case 1:
+            historyQuery.toLowerCase = "art history" || "art"
+            console.log("Art History selected");
+            return historyQuery + "selected";
+        case 2:
+            historyQuery.toLowerCase = "music history" || "music"
+            console.log("Music History selected");
+            return historyQuery + "selected";
+        case 3:
+            historyQuery.toLowerCase = "food history" || "food"
+            console.log("Food History selected");
+            return historyQuery + "selected";
+        case 4:
+            historyQuery.toLowerCase = "economic history" || "economics" || "money"
+            console.log("Economic History selected");
+            return historyQuery + "selected";
+        case 5:
+            historyQuery.toLowerCase = "science history" || "science"
+            console.log("Science History selected");
+            return historyQuery + "selected";
     }
 }
